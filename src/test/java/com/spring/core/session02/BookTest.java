d9785d7874b836f99f70d662a1a1327e26a19285
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-24 15:54:02 +0800

    Add files via upload

--- src/test/java/com/spring/core/session02/BookTest.java
+++ src/test/java/com/spring/core/session02/BookTest.java
@@ -11,14 +11,24 @@ public class BookTest {
 	@Test
 	public void test1() {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-		Author author = ctx.getBean("author", Author.class);
+		Author author2 = ctx.getBean("author2", Author.class);
 		Book book = ctx.getBean("book", Book.class);
 		System.out.println(book); // 資料尚未注入
 		book.setName("Java");
 		book.setPrice(500);
-		book.setAuthor(author);
+		book.setAuthor(author2);
 		System.out.println(book);
 		
+		Book book2 = ctx.getBean("book2", Book.class);
+		System.out.println(book2); // 資料已注入（方法）
 		
+		Book book3 = ctx.getBean("book3", Book.class);
+		System.out.println(book3); // 資料已注入（建構子）
+		
+		Book book4 = ctx.getBean("book4", Book.class);
+		System.out.println(book4); // 資料已注入（方法 p）
+		
+		Book book5 = ctx.getBean("book5", Book.class);
+		System.out.println(book5); // 資料已注入（建構子 c）
 	}
 }
