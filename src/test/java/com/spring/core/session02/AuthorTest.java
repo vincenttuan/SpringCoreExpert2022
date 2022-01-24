6bf3228e7b4a3d0d2e1eb7bf1dba3fbbd0a8271c
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-24 15:17:30 +0800

    Add files via upload

--- src/test/java/com/spring/core/session02/AuthorTest.java
+++ src/test/java/com/spring/core/session02/AuthorTest.java
@@ -12,11 +12,16 @@ public class AuthorTest {
 	public void test1() {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 		Author author = ctx.getBean("author", Author.class);
-		author.setName("Vincent");
-		author.setSex('男');
-		author.setAge(25);
+		System.out.println(author); // 資料尚未注入
+		author.setName("Vincent");  // 手動注入
+		author.setSex('男');        // 手動注入
+		author.setAge(25);          // 手動注入
 		System.out.println(author);
-		Author author2 = ctx.getBean("author2", Author.class);
-		System.out.println(author2);
+		
+		Author author2 = ctx.getBean("author2", Author.class); // 資料透過設定檔注入
+		System.out.println(author2); // 資料已注入（透過方法）
+		
+		Author author3 = ctx.getBean("author3", Author.class); // 資料透過設定檔注入
+		System.out.println(author3); // 資料已注入（透過建構子）
 	}
 }
