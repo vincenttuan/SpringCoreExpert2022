cdbe4c5f71c0478f5cf4978bd0d9b6b133f9011f
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-24 13:30:11 +0800

    Add files via upload

---
+++ src/main/java/com/spring/core/session01/beans/Hello.java
@@ -0,0 +1,12 @@
+package com.spring.core.session01.beans;
+
+import java.util.Date;
+
+public class Hello {
+
+	@Override
+	public String toString() {
+		return "Hello [" + new Date() + "]";
+	}
+	
+}
