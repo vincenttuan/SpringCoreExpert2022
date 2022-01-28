cb9896c7949a3b154ebeb08982b135016d01d2b6
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-28 15:15:27 +0800

    Add files via upload

--- src/test/java/com/spring/core/session05/AOPLabTest.java
+++ src/test/java/com/spring/core/session05/AOPLabTest.java
@@ -16,12 +16,12 @@ public class AOPLabTest {
 		ApplicationContext ctx = new AnnotationConfigApplicationContext(AOPConfig.class);
 		Performance dancer = ctx.getBean("dancer", Performance.class);
 		try {
-			dancer.perform();
+			dancer.perform(); // 執行表演
 		} catch (Exception e) {
-			System.out.println(e);
+			System.out.println(e); // 例外發生與處理
 			// 轉換跑道
 			// dancer 透過 introducter(經紀人) 轉換跑道
-			// 例如改行去唱歌或當主持人
+			// 例如：改行去唱歌
 			System.out.println("舞者轉歌者");
 			Singer singer = (Singer)dancer; // 舞者轉歌者
 			singer.sing();
---
+++ src/test/java/com/spring/core/session06/SQLiteCreateDatabaseAndConnectionTest.java
@@ -0,0 +1,25 @@
+package com.spring.core.session06;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class SQLiteCreateDatabaseAndConnectionTest {
+	
+	@Test
+	public void test() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-config.xml");
+		ComboPooledDataSource cp = ctx.getBean("dataSource", ComboPooledDataSource.class);
+		try {
+			System.out.println(cp.getConnection()); // 資料庫連線
+		} catch (SQLException e) {
+			System.out.println("資料庫建立連線失敗");
+			e.printStackTrace();
+			return;
+		}
+		System.out.println("資料庫建立連線成功");
+	}
+}
