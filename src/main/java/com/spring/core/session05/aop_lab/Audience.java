719e35dc6dd0a4afd656ac50a59a0dd66f1b8b81
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-28 13:46:09 +0800

    Update Audience.java

--- src/main/java/com/spring/core/session05/aop_lab/Audience.java
+++ src/main/java/com/spring/core/session05/aop_lab/Audience.java
@@ -1,5 +1,8 @@
 package com.spring.core.session05.aop_lab;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -11,12 +14,28 @@ public class Audience {
 	
 	@Before(value = "pt()")
 	public void slienceCellPhone() {
-		System.out.println("關掉手機");
+		System.out.println("觀眾-關掉手機");
 	}
 	
 	@Before(value = "pt()")
 	public void takeSeats() {
-		System.out.println("找到座位");
+		System.out.println("觀眾-找到座位");
 	}
+	
+	@After(value = "pt()")
+	public void applause() {
+		System.out.println("觀眾-拍手鼓掌");
+	}
+	
+	@AfterReturning(value = "pt()")
+	public void exit() {
+		System.out.println("觀眾-離開表演會場");
+	}
+	
+	@AfterThrowing(value = "pt()")
+	public void demandRefund() {
+		System.out.println("觀眾-退票！退票！並離場");
+	}
+	
 	
 }
